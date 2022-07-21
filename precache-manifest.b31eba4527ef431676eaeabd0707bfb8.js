4df7e8f74c66bf1f2b02be678a1c55fa5291a20c
rontab95 <rontab95@yahoo.com>
2022-07-20 22:10:25 -0700

    Updates

---
+++ precache-manifest.b31eba4527ef431676eaeabd0707bfb8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7a98f59bc710e2c1954",
+    "url": "/Sticky-App/static/css/main.1670b374.chunk.css"
+  },
+  {
+    "revision": "f7a98f59bc710e2c1954",
+    "url": "/Sticky-App/static/js/main.7f04bf35.chunk.js"
+  },
+  {
+    "revision": "deec573342ab5eeb915a",
+    "url": "/Sticky-App/static/js/runtime~main.0f148440.js"
+  },
+  {
+    "revision": "59fa5faa0d43ca05c280",
+    "url": "/Sticky-App/static/js/2.da757658.chunk.js"
+  },
+  {
+    "revision": "8b775076dc4830d85af343cec8586bb8",
+    "url": "/Sticky-App/index.html"
+  }
+];
