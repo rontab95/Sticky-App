c9e2f7fb1cfa9d661b2e878b8a2df86d5fde68a8
batron95 <rontab95@yahoo.com>
2025-01-06 21:40:20 -0800

    Initial gh-pages deployment

---
+++ precache-manifest.4911c2a1715f9be0d1babda9447821c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ebfd366856b85f2d9f7f",
+    "url": "/Sticky-App/static/css/main.6ac2c8a3.chunk.css"
+  },
+  {
+    "revision": "ebfd366856b85f2d9f7f",
+    "url": "/Sticky-App/static/js/main.0aa583a4.chunk.js"
+  },
+  {
+    "revision": "deec573342ab5eeb915a",
+    "url": "/Sticky-App/static/js/runtime~main.0f148440.js"
+  },
+  {
+    "revision": "59fa5faa0d43ca05c280",
+    "url": "/Sticky-App/static/js/2.da757658.chunk.js"
+  },
+  {
+    "revision": "520fe8486e75a0f91a5ee9f194b2fabc",
+    "url": "/Sticky-App/index.html"
+  }
+];
